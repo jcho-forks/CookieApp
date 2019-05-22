e7fac3b069b88380efb8e498828d31360be41a68
June Hyok Cho <jcho0627@gmail.com>
2019-05-22 11:23:26 +0200

    dummy ingredients data added

--- assets/data/cookies.js
+++ assets/data/cookies.js
@@ -2,91 +2,271 @@ export const cookies = [
   {
     name: "Cookie 1",
     image: "https://images.unsplash.com/photo-1532499016263-f2c3e89de9cd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni corporis eos, voluptas officia hic dicta? Lorem ipsum dolor sit amet consectetur adipisicing elit.Rem, nemo."
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni corporis eos, voluptas officia hic dicta? Lorem ipsum dolor sit amet consectetur adipisicing elit.Rem, nemo.",
+    ingredients: [
+      "2 cups diced cooked turkey",
+      "2 celery ribs, diced",
+      "1 small onion, diced",
+      "2 hard-cooked eggs, chopped",
+      "3/4 cup mayonnaise",
+      "1/2 teaspoon salt",
+      "1/4 teaspoon pepper",
+      "6 hamburger buns, split"
+    ]
   },
   {
     name: "Cookie 2",
     image: "https://images.unsplash.com/photo-1530751127259-074b0cdc0469?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni corporis eos, voluptas officia hic dicta? Lorem ipsum dolor sit amet consectetur adipisicing elit.Rem, nemo."
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni corporis eos, voluptas officia hic dicta? Lorem ipsum dolor sit amet consectetur adipisicing elit.Rem, nemo.",
+    ingredients: [
+      "2 cups diced cooked turkey",
+      "2 celery ribs, diced",
+      "1 small onion, diced",
+      "2 hard-cooked eggs, chopped",
+      "3/4 cup mayonnaise",
+      "1/2 teaspoon salt",
+      "1/4 teaspoon pepper",
+      "6 hamburger buns, split"
+    ]
   },
   {
     name: "Cookie 3",
     image: "https://images.unsplash.com/photo-1548365328-8c6db3220e4c?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni corporis eos, voluptas officia hic dicta? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur suscipit similique praesentium dolorum veniam. Enim aliquid, ad ullam tempore consequuntur eos pariatur amet porro voluptas iusto molestias nihil omnis."
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni corporis eos, voluptas officia hic dicta? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur suscipit similique praesentium dolorum veniam. Enim aliquid, ad ullam tempore consequuntur eos pariatur amet porro voluptas iusto molestias nihil omnis.",
+    ingredients: [
+      "2 cups diced cooked turkey",
+      "2 celery ribs, diced",
+      "1 small onion, diced",
+      "2 hard-cooked eggs, chopped",
+      "3/4 cup mayonnaise",
+      "1/2 teaspoon salt",
+      "1/4 teaspoon pepper",
+      "6 hamburger buns, split"
+    ]
   },
   {
     name: "Cookie 4",
     image: "https://images.unsplash.com/photo-1491374812364-00028bbe7d2f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias provident vero voluptatum consequatur fugit saepe itaque autem cupiditate, est mollitia error unde asperiores praesentium eveniet tempore ipsum culpa repudiandae. Eaque a aperiam cupiditate ratione id praesentium vitae, quam quae."
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias provident vero voluptatum consequatur fugit saepe itaque autem cupiditate, est mollitia error unde asperiores praesentium eveniet tempore ipsum culpa repudiandae. Eaque a aperiam cupiditate ratione id praesentium vitae, quam quae.",
+    ingredients: [
+      "2 cups diced cooked turkey",
+      "2 celery ribs, diced",
+      "1 small onion, diced",
+      "2 hard-cooked eggs, chopped",
+      "3/4 cup mayonnaise",
+      "1/2 teaspoon salt",
+      "1/4 teaspoon pepper",
+      "6 hamburger buns, split"
+    ]
   },
   {
     name: "Cookie 5",
     image: "https://images.unsplash.com/photo-1515265705-cbcc6e6ba264?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni corporis eos, voluptas officia hic dicta? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur suscipit similique praesentium dolorum veniam. Enim aliquid, ad ullam tempore consequuntur eos pariatur amet porro voluptas iusto molestias nihil omnis."
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni corporis eos, voluptas officia hic dicta? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur suscipit similique praesentium dolorum veniam. Enim aliquid, ad ullam tempore consequuntur eos pariatur amet porro voluptas iusto molestias nihil omnis.",
+    ingredients: [
+      "2 cups diced cooked turkey",
+      "2 celery ribs, diced",
+      "1 small onion, diced",
+      "2 hard-cooked eggs, chopped",
+      "3/4 cup mayonnaise",
+      "1/2 teaspoon salt",
+      "1/4 teaspoon pepper",
+      "6 hamburger buns, split"
+    ]
   },
   {
     name: "Cookie 6",
     image: "https://images.unsplash.com/photo-1513806562344-c75007ad303f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum corrupti fuga quaerat perspiciatis, enim dolorum aperiam aut? Quas obcaecati facere, quasi nostrum iure atque ipsam, debitis ut aspernatur pariatur, dolorem natus odit minima! Voluptatem fuga esse ab ullam cumque debitis reprehenderit quidem non nihil accusamus?"
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum corrupti fuga quaerat perspiciatis, enim dolorum aperiam aut? Quas obcaecati facere, quasi nostrum iure atque ipsam, debitis ut aspernatur pariatur, dolorem natus odit minima! Voluptatem fuga esse ab ullam cumque debitis reprehenderit quidem non nihil accusamus?",
+    ingredients: [
+      "2 cups diced cooked turkey",
+      "2 celery ribs, diced",
+      "1 small onion, diced",
+      "2 hard-cooked eggs, chopped",
+      "3/4 cup mayonnaise",
+      "1/2 teaspoon salt",
+      "1/4 teaspoon pepper",
+      "6 hamburger buns, split"
+    ]
   },
   {
     name: "Cookie 7",
     image: "https://images.unsplash.com/photo-1532499016263-f2c3e89de9cd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni corporis eos, voluptas officia hic dicta? Lorem ipsum dolor sit amet consectetur adipisicing elit.Rem, nemo."
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni corporis eos, voluptas officia hic dicta? Lorem ipsum dolor sit amet consectetur adipisicing elit.Rem, nemo.",
+    ingredients: [
+      "2 cups diced cooked turkey",
+      "2 celery ribs, diced",
+      "1 small onion, diced",
+      "2 hard-cooked eggs, chopped",
+      "3/4 cup mayonnaise",
+      "1/2 teaspoon salt",
+      "1/4 teaspoon pepper",
+      "6 hamburger buns, split"
+    ]
   },
   {
     name: "Cookie 8",
     image: "https://images.unsplash.com/photo-1530751127259-074b0cdc0469?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni corporis eos, voluptas officia hic dicta? Lorem ipsum dolor sit amet consectetur adipisicing elit.Rem, nemo."
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni corporis eos, voluptas officia hic dicta? Lorem ipsum dolor sit amet consectetur adipisicing elit.Rem, nemo.",
+    ingredients: [
+      "2 cups diced cooked turkey",
+      "2 celery ribs, diced",
+      "1 small onion, diced",
+      "2 hard-cooked eggs, chopped",
+      "3/4 cup mayonnaise",
+      "1/2 teaspoon salt",
+      "1/4 teaspoon pepper",
+      "6 hamburger buns, split"
+    ]
   },
   {
     name: "Cookie 9",
     image: "https://images.unsplash.com/photo-1548365328-8c6db3220e4c?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni corporis eos, voluptas officia hic dicta? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur suscipit similique praesentium dolorum veniam. Enim aliquid, ad ullam tempore consequuntur eos pariatur amet porro voluptas iusto molestias nihil omnis."
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni corporis eos, voluptas officia hic dicta? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur suscipit similique praesentium dolorum veniam. Enim aliquid, ad ullam tempore consequuntur eos pariatur amet porro voluptas iusto molestias nihil omnis.",
+    ingredients: [
+      "2 cups diced cooked turkey",
+      "2 celery ribs, diced",
+      "1 small onion, diced",
+      "2 hard-cooked eggs, chopped",
+      "3/4 cup mayonnaise",
+      "1/2 teaspoon salt",
+      "1/4 teaspoon pepper",
+      "6 hamburger buns, split"
+    ]
   },
   {
     name: "Cookie 10",
     image: "https://images.unsplash.com/photo-1491374812364-00028bbe7d2f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias provident vero voluptatum consequatur fugit saepe itaque autem cupiditate, est mollitia error unde asperiores praesentium eveniet tempore ipsum culpa repudiandae. Eaque a aperiam cupiditate ratione id praesentium vitae, quam quae."
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias provident vero voluptatum consequatur fugit saepe itaque autem cupiditate, est mollitia error unde asperiores praesentium eveniet tempore ipsum culpa repudiandae. Eaque a aperiam cupiditate ratione id praesentium vitae, quam quae.",
+    ingredients: [
+      "2 cups diced cooked turkey",
+      "2 celery ribs, diced",
+      "1 small onion, diced",
+      "2 hard-cooked eggs, chopped",
+      "3/4 cup mayonnaise",
+      "1/2 teaspoon salt",
+      "1/4 teaspoon pepper",
+      "6 hamburger buns, split"
+    ]
   },
   {
     name: "Cookie 11",
     image: "https://images.unsplash.com/photo-1515265705-cbcc6e6ba264?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni corporis eos, voluptas officia hic dicta? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur suscipit similique praesentium dolorum veniam. Enim aliquid, ad ullam tempore consequuntur eos pariatur amet porro voluptas iusto molestias nihil omnis."
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni corporis eos, voluptas officia hic dicta? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur suscipit similique praesentium dolorum veniam. Enim aliquid, ad ullam tempore consequuntur eos pariatur amet porro voluptas iusto molestias nihil omnis.",
+    ingredients: [
+      "2 cups diced cooked turkey",
+      "2 celery ribs, diced",
+      "1 small onion, diced",
+      "2 hard-cooked eggs, chopped",
+      "3/4 cup mayonnaise",
+      "1/2 teaspoon salt",
+      "1/4 teaspoon pepper",
+      "6 hamburger buns, split"
+    ]
   },
   {
     name: "Cookie 12",
     image: "https://images.unsplash.com/photo-1513806562344-c75007ad303f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum corrupti fuga quaerat perspiciatis, enim dolorum aperiam aut? Quas obcaecati facere, quasi nostrum iure atque ipsam, debitis ut aspernatur pariatur, dolorem natus odit minima! Voluptatem fuga esse ab ullam cumque debitis reprehenderit quidem non nihil accusamus?"
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum corrupti fuga quaerat perspiciatis, enim dolorum aperiam aut? Quas obcaecati facere, quasi nostrum iure atque ipsam, debitis ut aspernatur pariatur, dolorem natus odit minima! Voluptatem fuga esse ab ullam cumque debitis reprehenderit quidem non nihil accusamus?",
+    ingredients: [
+      "2 cups diced cooked turkey",
+      "2 celery ribs, diced",
+      "1 small onion, diced",
+      "2 hard-cooked eggs, chopped",
+      "3/4 cup mayonnaise",
+      "1/2 teaspoon salt",
+      "1/4 teaspoon pepper",
+      "6 hamburger buns, split"
+    ]
   },
   {
     name: "Cookie 13",
     image: "https://images.unsplash.com/photo-1532499016263-f2c3e89de9cd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni corporis eos, voluptas officia hic dicta? Lorem ipsum dolor sit amet consectetur adipisicing elit.Rem, nemo."
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni corporis eos, voluptas officia hic dicta? Lorem ipsum dolor sit amet consectetur adipisicing elit.Rem, nemo.",
+    ingredients: [
+      "2 cups diced cooked turkey",
+      "2 celery ribs, diced",
+      "1 small onion, diced",
+      "2 hard-cooked eggs, chopped",
+      "3/4 cup mayonnaise",
+      "1/2 teaspoon salt",
+      "1/4 teaspoon pepper",
+      "6 hamburger buns, split"
+    ]
   },
   {
     name: "Cookie 14",
     image: "https://images.unsplash.com/photo-1530751127259-074b0cdc0469?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni corporis eos, voluptas officia hic dicta? Lorem ipsum dolor sit amet consectetur adipisicing elit.Rem, nemo."
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni corporis eos, voluptas officia hic dicta? Lorem ipsum dolor sit amet consectetur adipisicing elit.Rem, nemo.",
+    ingredients: [
+      "2 cups diced cooked turkey",
+      "2 celery ribs, diced",
+      "1 small onion, diced",
+      "2 hard-cooked eggs, chopped",
+      "3/4 cup mayonnaise",
+      "1/2 teaspoon salt",
+      "1/4 teaspoon pepper",
+      "6 hamburger buns, split"
+    ]
   },
   {
     name: "Cookie 15",
     image: "https://images.unsplash.com/photo-1548365328-8c6db3220e4c?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni corporis eos, voluptas officia hic dicta? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur suscipit similique praesentium dolorum veniam. Enim aliquid, ad ullam tempore consequuntur eos pariatur amet porro voluptas iusto molestias nihil omnis."
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni corporis eos, voluptas officia hic dicta? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur suscipit similique praesentium dolorum veniam. Enim aliquid, ad ullam tempore consequuntur eos pariatur amet porro voluptas iusto molestias nihil omnis.",
+    ingredients: [
+      "2 cups diced cooked turkey",
+      "2 celery ribs, diced",
+      "1 small onion, diced",
+      "2 hard-cooked eggs, chopped",
+      "3/4 cup mayonnaise",
+      "1/2 teaspoon salt",
+      "1/4 teaspoon pepper",
+      "6 hamburger buns, split"
+    ]
   },
   {
     name: "Cookie 16",
     image: "https://images.unsplash.com/photo-1491374812364-00028bbe7d2f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias provident vero voluptatum consequatur fugit saepe itaque autem cupiditate, est mollitia error unde asperiores praesentium eveniet tempore ipsum culpa repudiandae. Eaque a aperiam cupiditate ratione id praesentium vitae, quam quae."
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias provident vero voluptatum consequatur fugit saepe itaque autem cupiditate, est mollitia error unde asperiores praesentium eveniet tempore ipsum culpa repudiandae. Eaque a aperiam cupiditate ratione id praesentium vitae, quam quae.",
+    ingredients: [
+      "2 cups diced cooked turkey",
+      "2 celery ribs, diced",
+      "1 small onion, diced",
+      "2 hard-cooked eggs, chopped",
+      "3/4 cup mayonnaise",
+      "1/2 teaspoon salt",
+      "1/4 teaspoon pepper",
+      "6 hamburger buns, split"
+    ]
   },
   {
     name: "Cookie 17",
     image: "https://images.unsplash.com/photo-1515265705-cbcc6e6ba264?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni corporis eos, voluptas officia hic dicta? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur suscipit similique praesentium dolorum veniam. Enim aliquid, ad ullam tempore consequuntur eos pariatur amet porro voluptas iusto molestias nihil omnis."
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni corporis eos, voluptas officia hic dicta? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur suscipit similique praesentium dolorum veniam. Enim aliquid, ad ullam tempore consequuntur eos pariatur amet porro voluptas iusto molestias nihil omnis.",
+    ingredients: [
+      "2 cups diced cooked turkey",
+      "2 celery ribs, diced",
+      "1 small onion, diced",
+      "2 hard-cooked eggs, chopped",
+      "3/4 cup mayonnaise",
+      "1/2 teaspoon salt",
+      "1/4 teaspoon pepper",
+      "6 hamburger buns, split"
+    ]
   },
   {
     name: "Cookie 18",
     image: "https://images.unsplash.com/photo-1513806562344-c75007ad303f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum corrupti fuga quaerat perspiciatis, enim dolorum aperiam aut? Quas obcaecati facere, quasi nostrum iure atque ipsam, debitis ut aspernatur pariatur, dolorem natus odit minima! Voluptatem fuga esse ab ullam cumque debitis reprehenderit quidem non nihil accusamus?"
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum corrupti fuga quaerat perspiciatis, enim dolorum aperiam aut? Quas obcaecati facere, quasi nostrum iure atque ipsam, debitis ut aspernatur pariatur, dolorem natus odit minima! Voluptatem fuga esse ab ullam cumque debitis reprehenderit quidem non nihil accusamus?",
+    ingredients: [
+      "2 cups diced cooked turkey",
+      "2 celery ribs, diced",
+      "1 small onion, diced",
+      "2 hard-cooked eggs, chopped",
+      "3/4 cup mayonnaise",
+      "1/2 teaspoon salt",
+      "1/4 teaspoon pepper",
+      "6 hamburger buns, split"
+    ]
   }
 ];
